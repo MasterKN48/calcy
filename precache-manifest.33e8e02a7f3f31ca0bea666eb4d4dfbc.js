26ab56532173513c38a4206165ceb1ee7e0ef0f9
MasterKN <masterkn48@gmail.com>
2019-10-01 01:32:23 +0530

    Updates

---
+++ precache-manifest.33e8e02a7f3f31ca0bea666eb4d4dfbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976de89d2a6c370d4301c0f8916c941d",
+    "url": "/calcy/index.html"
+  },
+  {
+    "revision": "71182db19dff5ca8d6db",
+    "url": "/calcy/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "698a87f54954e11ff1c2",
+    "url": "/calcy/static/css/main.db6cfe94.chunk.css"
+  },
+  {
+    "revision": "71182db19dff5ca8d6db",
+    "url": "/calcy/static/js/2.dc2891d6.chunk.js"
+  },
+  {
+    "revision": "698a87f54954e11ff1c2",
+    "url": "/calcy/static/js/main.cbefb65f.chunk.js"
+  },
+  {
+    "revision": "e7f79425c6a84d2289d9",
+    "url": "/calcy/static/js/runtime-main.12c0e713.js"
+  },
+  {
+    "revision": "8d495829375b405f355f06f37fdef644",
+    "url": "/calcy/static/media/logo.8d495829.png"
+  }
+]);
